71da18dd183418809ca32324db3750f0cec12659
Xanzik <pustovoi2005@gmail.com>
2021-12-20 12:59:59 +0200

    GG

--- lab07/src/main.c
+++ lab07/src/main.c
@@ -1,13 +1,21 @@
-#include <iostream>
 #include <stdlib.h>
-using namespace std;
-
+#include <stdio.h>
+#include <time.h>
 int func(int x);
-int main() {
-    int y = rand();
-    y = func(y);
-
-    return 0;
+int main(int argc, char *argv[]) {
+	int y;
+	if(argv[1] != 0) {
+		y = atoi(argv[1]);
+		y = func(y);
+		printf("result = %d\n", y);
+	}
+	else {
+		srand(time(NULL));
+		y = 1 + rand() % 100;
+		y = func(y);
+		printf("result = %d\n", y);
+	}
+	return 0;
 }
 
 int func(int x) {
@@ -19,22 +27,36 @@ int func(int x) {
         }
     return x;
 }
-
-/*char function(char x[]);
-int main() {
-    int words;
-    char text[] = {'I', ' ', 'c', 'a', 'n', ' ', 'f', 'i', 'g', 'h', 't'};
-    words = function(text);
-    return 0;
+/*
+char function(char x[]);
+int main(int argc, char* argv[]) {
+	int words;
+	if(argc != 1) {
+		words = function(argv);
+		words *= argc - 1;
+		printf("Words = %d\n", words);
+	}
+	else {
+		char text[] = {' ', 'I', ' ', '.', '.', ' ', 'c', 'a', 'n', ' ', 'f', 'i', 'g', 'h', 't', '!', ' ', '\n'};
+		words = function(text);
+		printf("Words = %d\n", words);
+	}
+	return 0;
 }
-
 char function(char x[]) {
-    int words = 1;
-    int limit = 255;
+    int limit = 50;
+    int words = 0;
+	if(x[0] != ' ') {
+		words++;
+}
     for(int i = 1; i < limit; i++) {
-        if(x[i] == ' ')
-        if(x[i + 1] != ' ')
-            words ++;
-    }
+        if(x[i] == ' ') {
+        	if(x[i + 1] != ' ' && x[i + 1] != '\n')
+			words ++;
+	}
+	if(x[i] == '.' && x[i] == '!' && x[i] == ',' && x[i] == '!')
+		words--;
+       }
     return words;
-} */
+}
+ */
